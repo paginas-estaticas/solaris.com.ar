7b894fcf86d6e6b1c9fe18a0e833311b10e8f3a4
Felipe <felipewellsmusic@gmail.com>
2024-12-03 20:53:40 -0300

    sections and nav menu

--- Scripts/hamburgerMenu.js
+++ Scripts/hamburgerMenu.js
@@ -1,14 +1,17 @@
+const headerElement = document.getElementsByTagName("header")[0];
 const hamburgerMenu = document.getElementById("hamburguer-menu-icon");
 const navMenu = document.getElementsByTagName("nav")[0];
 
 
 hamburgerMenu.addEventListener("click", () => {
     if (hamburgerMenu.classList.contains("active")) {
+        headerElement.classList.remove("active")
         hamburgerMenu.classList.remove("active");
         navMenu.classList.remove("active");
     } else {
-        hamburgerMenu.classList.toggle("active");
-        navMenu.classList.toggle("active");
+        headerElement.classList.add("active")
+        hamburgerMenu.classList.add("active");
+        navMenu.classList.add("active");
     } 
 });
 
@@ -19,6 +22,7 @@ function delay(seconds) {
 document.querySelectorAll(".nav-li-element").forEach(element => 
     element.addEventListener("click", async () => {
         await delay(2000)
+        headerElement.classList.remove("active")
         hamburgerMenu.classList.remove("active");
         navMenu.classList.remove("active");
     }));
